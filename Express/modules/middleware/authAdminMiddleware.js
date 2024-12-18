13aca226ac56e54c8d17c6495c65ad6150d1b6e8
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-12-18 17:41:25 +0530

    Delete Express directory

--- Express/modules/middleware/authAdminMiddleware.js
+++
@@ -1,9 +0,0 @@
-function adminMiddleware(req, res, next) {
-    if (req.user.isAdmin === 1) {
-        next();
-    } else {
-        res.status(403).json({ message: 'Access forbidden. Admin privileges required.' });
-    }
-}
-
-module.exports = adminMiddleware;
